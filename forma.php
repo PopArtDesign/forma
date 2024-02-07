f9ffe72bc60d2628fe03ea706479fd0d6bc44f2e
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2024-02-07 19:33:43 +0300

    Use PHPMailer

--- forma.php
+++ forma.php
@@ -2,9 +2,12 @@
 
 namespace PopArtDesign\Forma;
 
-use function preg_replace;
-
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/helpers.php';
+
+use function preg_replace;
+use PHPMailer\PHPMailer\DSNConfigurator;
+use PHPMailer\PHPMailer\PHPMailer;
 
 if ('imnotarobot!' !== getRequest('_secret')) {
     jsendFail(['message' => 'Некорректное значение антиспам-поля!']);
@@ -15,29 +18,36 @@ $phone = preg_replace('/[^+0-9]/', '', getRequest('phone'));
 $attachments = collectAttachments(['files']);
 
 if (!$name || !$phone || count($attachments) == 0) {
-    jsendFail(['message' => 'Пожалуйста, заполните все обязательные поля!']);
+    jsendFail(['message' => 'Правильно заполните все обязательные поля!']);
 }
 
 $domain = detectDomain();
 
-$to      = 'user@localhost';
+$to      = 'user@localhost.local';
 $from    = 'no-reply@' . $domain;
 $subject = 'Сообщение с сайта '. $domain;
-
 $message = loadTemplate(__DIR__ . '/email.php', [
     'name' => $name,
     'phone' => $phone,
 ]);
 
-$options = [
-    'from' => $from,
-    'sender' => $from,
-    'attachments' => $attachments,
-    'html' => true,
-];
+try {
+    $mail = DSNConfigurator::mailer('mail://localhost', true);
 
-if (sendMail($to, $subject, $message, $options)) {
+    $mail->addAddress($to);
+    $mail->setFrom($from);
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    $mail->CharSet = PHPMailer::CHARSET_UTF8;
+    $mail->isHtml(true);
+
+    foreach ($attachments as $attachment) {
+        $mail->addAttachment($attachment['path'], $attachment['name']);
+    }
+
+    $mail->send();
+
     jsendSuccess();
-} else {
+} catch (\Exception $e) {
     jsendError();
 }
